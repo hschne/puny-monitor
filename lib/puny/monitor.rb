f60985b5df756c7906e53c17980cefa60e4a0a62
hschne <hans.schnedlitz@gmail.com>
2024-09-15 07:20:42 +0200

    Initial commit

---
+++ lib/puny/monitor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "monitor/version"
+
+module Puny
+  module Monitor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/puny/monitor/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Puny
+  module Monitor
+    VERSION = "0.1.0"
+  end
+end
